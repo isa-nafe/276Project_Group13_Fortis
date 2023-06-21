199d2a2310ed8bb1603ce6f6ddc39b49cdce4b3b
kirillous <kirill.markin18@gmail.com>
2023-06-21 11:45:50 -0700

    Empty Fortis form project

---
+++ src/main/java/com/fortis_form/fortis_form/FortisFormApplication.java
@@ -0,0 +1,13 @@
+package com.fortis_form.fortis_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortisFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortisFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fortis_form/fortis_form/FortisFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortis_form.fortis_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortisFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
