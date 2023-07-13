515977f6beaa71ab41ead33177ac6e220cfc54bf
yuriicoding <itdevelopment2021@gmail.com>
2023-07-12 18:53:10 -0700

    email smtp set up

--- src/main/resources/static/js/nonuser-pdf.js
+++ src/main/resources/static/js/nonuser-pdf.js
@@ -13,4 +13,25 @@ docPDF.html(elementHTML, {
  width: 170,
  windowWidth: 650
 });
+
+
 }
+
+function sendEmail() {
+	Email.send({
+		Host: "smtp.elasticemail.com",
+		Username : "emergency-form@cmpt276.com",
+		Password : "845BC7CA4184E311B4A8511564D6C1A1B4A7",
+		To : 'ivanpostolov03@mail.ru',
+		From : 'wertysytre@gmail.com', // put a real email address that is verified
+		Port:'2525',
+		Subject : "Hello",
+		Body : "Hello",
+	}).then(
+		message => {
+			alert("mail sent successfully");
+			console.log(message);
+		}
+	);
+}
+
