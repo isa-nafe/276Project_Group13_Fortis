f994449fb39c4220b5967cd9247ab76ad05c642a
Isa <isa.nafieiev@gmail.com>
2023-07-13 17:05:57 -0700

    nonuser-questions DB dictionary

--- src/main/resources/static/js/nonuser-questions.js
+++ src/main/resources/static/js/nonuser-questions.js
@@ -1,3 +1,39 @@
+var Dialogue_Boxes_Dictionary = {
+   DB1: "CONFIRM the following information with the customer by READING BACK:\n1) CALLER or CONTACT TELEPHONE NUMBERn2) CALLER or CONTACT NAME\n3) Address given by caller - CITY, TOWN, STREET NAME, CROSS STREETS\n4) Advise caller that someone MUST BE ON SITE to meet the technician",
+
+   DB2: "CSR:  Confirm with caller if multiple appliances are not working or if a single appliance is not working\n- If multiple appliances are not working, cancel form and create meter investigate “urgent” service order.  (If after hours (4:00 pm – 7:30 am), phone Dispatch at 613-745-1576 option 7)\n- If single appliance only, advise customer:   Check your telephone listings for a licensed gas fitter, and arrange to have your gas appliances inspected and serviced if required.",
+   
+   DB9: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate.  The technician will make the situation safe; however, they will not make repairs to appliances.  Here are some safety precautions to follow until we arrive.\n\nDO NOT:\n-Light matches or cigarettes\n-Turn your electric lights ON or OFF\n-Use your electric appliances\n-Use the telephone after this call at the location of the emergency\n\nDO:\n-Open doors and windows to allow ventilation\n-Leave building, ensuring someone will be on-site to meet the technician and provide access\n\n*When technician arrives gas will be shut off if they cannot gain access:\n\nIf the appliance is: a gas dryer, range, stove or fireplace - SWITCH OFF IMMEDIATELY\n\nIf a furnace:  TURN THERMOSTAT DOWN UNTIL  FURNACE SWITCHES OFF\n\nPhone Zone Dispatch at 613-745-1576",
+
+   DB11: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate. The technician will make the situation safe; however, they will not make repairs to appliances.  Here are some safety precautions to follow until we arrive.\n\nCALL emergency line.\n\nDO NOT:\n-Light matches or cigarettes\n-Turn your electric lights ON or OFF\n-Use any appliances\n-Use the telephone after this call at the location of the emergency\n\nDO:\n-Open doors and windows to allow ventilation\n-Leave building, ensuring someone will be on-site to meet the technician and provide access\n\n*When technician arrives gas will be shut off if they cannot gain access.\n\nIf the appliance is: a gas dryer, range, stove or fireplace - SWITCH OFF IMMEDIATELY\n\nIf a furnace:  TURN THERMOSTAT DOWN UNTIL FURNACE SWITCHES OFF\n\nPhone Zone Dispatch at 613-745-1576",
+
+   DB13: "Advise Caller - We will dispatch a technician TODAY to investigate.  The technican will make the situation safe; however, they will not make repairs to appliances.  Here are some safety precautions to follow until we arrive.\n\nTurn the thermostat to the lowest setting.\n\n*Ensure that someone will be on site to meet the technician\n*When technician arrives gas will be shut off if they cannot gain access.",
+
+   DB14: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate.\n\nIf Fire Department asks for an ETA, advise them that (IRM Dispatch)(SR6707)will call them back and make sure  (IRM Dispatch)(SR6707) is aware to call back\n\nPhone Zone Dispatch at 613-745-1576",
+   
+   DB15: "Advise Caller - We will dispatch a technician TODAY to investigate.\n\nCSR: CANCEL FORM AND CREATE METER INVESTIGATE “URGENT”  SERVICE ORDER ",
+   
+   DB16: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate. Here are some safety precautions until we arrive.\n\nOpen ALL Doors and Windows.\n-Leave the building\n- If there are other people living at the premise, knock on their doors to advise them to also evacuate but DO NOT use a phone to call them\n-Seek Medical Attention if experiencing headache, nausea, dizziness, tiredness or flu-like symptoms\n-Call emergency if necessary\n\n*Ensure that someone will be on site to meet the technician\n*When technician arrives gas will be shut off if they cannot gain access.\n\nPLEASE NOTE:\nIf the technician is expecting someone to answer the door and nobody does, the technician may ask the Fire Department to gain access to the premise.\n\nPhone Zone Dispatch at 613-745-1576",
+
+   DB17: "Customer Information Only\n\nPlease check your telephone listings for a licensed gas fitter, and arrange to have your gas appliances inspected and serviced if required.",
+
+   DB18: "Requestion Info about appliance safety…or responding to media/advertising:\n\nSend caller literature on CO and appliance maintenance",
+   
+   DB19: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate.\n\nKeep people and sources of ignition clear of the excavation and escaping gas until our Technician arrives.\n\nPhone Zone Dispatch at 613-745-1576",
+
+   DB20: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate.\n\nKeep people and sources of ignition clear of the excavation and any escaping gas until our Technician arrives.\n\nLeave everything as is; if it is on, leave it on, if it is off, leave it off.\n\nDO NOT:\n-Light matches or cigarettes\n-Turn your lights ON or OFF\n-Use any appliances\n-Use the telephone after this call at the location of the emergency\n\nDO:\n-Open doors and windows to allow ventilation\n-Leave building, ensuring someone will be on-site to meet the technician and provide access\n\nWhen tech arrives gas will be shut off if they cannot gain access.\n\nPhone Zone Dispatch at 613-745-1576",
+   
+   DB22: "Please advise caller to contact BC ONE Call at 1 800 474 6886",
+
+   DB23: "Advise Caller - We will dispatch a technician TODAY to investigate.\nHere are some safety precautions to follow until we arrive.\n\nDO NOT:\n-Light matches or cigarettes\n-Use anything that could create a spark or flame, including vehicles, telephones, and machinery\n-Undertake any corrective action\n\nDO:\n-Stay clear of the affected area, but remain nearby in a safe location to meet the technician\n-Call back with a contact telephone number if evacuating the area\n-Keep doors and windows closed to prevent gas from entering the building\n-If you begin to smell gas inside the building you should ventilate on the opposite side from where the gas leak is located.\n\nWhen tech arrives gas will be shut off at the meter if they cannot gain access",
+
+   DB25: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate. (If caller is no longer at the location or is a meter reader, FortisBC contractor or dispatcher DO NOT READ OUT THE SAFETY PRECAUTIONS)  Otherwise advise the caller:  Here are some safety precautions to follow until we arrive.\n\nDO NOT:\n-Light matches or cigarettes\n-Use anything that could create a spark or flame, including vehicles, telephones, and machinery\n-Undertake any corrective action\n\nDO:\n-Stay clear of the affected area, but remain nearby in a safe location to meet the technician\n-Call back with a contact telephone number if evacuating the area\n-Keep doors and windows closed to prevent gas from entering the building\n-If you begin to smell gas inside the building, you should ventilate on the opposite side from where the odour is coming from\n\nWhen tech arrives gas will be shut off if they cannot gain access.\n\nPhone Zone Dispatch at 613-745-1576",
+   
+   DB26: "Advise Caller - We will dispatch a technician TODAY to investigate",
+
+
+}
+
 $(document).ready(function() {
 
    
