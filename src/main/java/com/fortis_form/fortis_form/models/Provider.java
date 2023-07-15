2dee05f2ec4259911aee67124ae35fae220c0419
Isa <isa.nafieiev@gmail.com>
2023-07-15 13:49:32 -0700

    google button (no html)

---
+++ src/main/java/com/fortis_form/fortis_form/models/Provider.java
@@ -0,0 +1,7 @@
+package com.fortis_form.fortis_form.models;
+
+public enum Provider {
+    LOCAL,
+    GOOGLE,
+    FACEBOOK
+}
--- src/main/java/com/fortis_form/fortis_form/models/User.java
+++ src/main/java/com/fortis_form/fortis_form/models/User.java
@@ -1,5 +1,28 @@
 package com.fortis_form.fortis_form.models;
 
+<<<<<<< Updated upstream
+=======
+import jakarta.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Entity
+@Table(name="users")
+>>>>>>> Stashed changes
 public class User {
+
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
 
 }
