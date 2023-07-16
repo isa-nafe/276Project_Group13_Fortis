485d8ffc529d64fc8106b8933b00f4d44d78cfb4
Isa <isa.nafieiev@gmail.com>
2023-07-16 16:04:26 -0700

    Google Login Button

--- src/main/resources/static/js/nonuser-questions.js
+++ src/main/resources/static/js/nonuser-questions.js
@@ -1,123 +1,39 @@
 var Dialogue_Boxes_Dictionary = {
-    DB8: "If multiple appliances are not working, cancel form and create meter investigate “urgent” service order. <br><br>If after hours (4:00 pm – 7:30 am), phone Dispatch at 613-745-1576, with option 7 <br><br>If single appliance only, then check your telephone listings for a licensed gas fitter, and arrange to have your gas appliances inspected and serviced if required.",
+  DB1: "CONFIRM the following information with the customer by READING BACK:\n1) CALLER or CONTACT TELEPHONE NUMBERn2) CALLER or CONTACT NAME\n3) Address given by caller - CITY, TOWN, STREET NAME, CROSS STREETS\n4) Advise caller that someone MUST BE ON SITE to meet the technician",
 
-    DB9: "We will IMMEDIATELY dispatch a technician to investigate. The technician will make the situation safe; however, they will not make repairs to appliances. <br>Here are some safety precautions to follow until we arrive. <br><br>Immediately CALL emergency line. <br><br>DO NOT: <br>-light matches or cigarettes <br>-turn your electric lights ON or OFF <br>-use any appliances <br>-use the telephone after this call at the location of the emergency <br><br>DO:<br>-open doors and windows to allow ventilation<br>-leave building, ensuring someone will be on-site to meet the technician and provide access <br><br>*When technician arrives gas will be shut off if they cannot gain access. <br><br>If the appliance is: a gas dryer, range, stove or fireplace - SWITCH OFF IMMEDIATELY. <br><br>If a furnace:  TURN THERMOSTAT DOWN UNTIL FURNACE SWITCHES OFF. <br><br>Phone Zone Dispatch at 613-745-1576.",
-  
-    DB11: "We will IMMEDIATELY dispatch a technician to investigate. The technician will make the situation safe; however, they will not make repairs to appliances. <br>Here are some safety precautions to follow until we arrive. <br><br>Immediately CALL emergency line. <br><br>DO NOT: <br>-light matches or cigarettes <br>-turn your electric lights ON or OFF <br>-use any appliances <br>-use the telephone after this call at the location of the emergency <br><br>DO:<br>-open doors and windows to allow ventilation<br>-leave building, ensuring someone will be on-site to meet the technician and provide access <br><br>*When technician arrives gas will be shut off if they cannot gain access. <br><br>If the appliance is: a gas dryer, range, stove or fireplace - SWITCH OFF IMMEDIATELY. <br><br>If a furnace:  TURN THERMOSTAT DOWN UNTIL FURNACE SWITCHES OFF. <br><br>Phone Zone Dispatch at 613-745-1576.",
-  
-    DB13: "We will dispatch a technician TODAY to investigate. The technican will make the situation safe; however, they will not make repairs to appliances. <br><br>Here are some safety precautions to follow until we arrive: <br>-turn the thermostat to the lowest setting <br>-ensure that someone will be on site to meet the technician <br>-when technician arrives gas will be shut off if they cannot gain access",
-  
-    DB14: "We will IMMEDIATELY dispatch a Technician to investigate. <br><br>If you are a Fortis Specialist, then follow this instructions: <br>-if Fire Department asks for an Estimated Time of Arrival, advise them that Technician will call them back <br>-make sure Technician is aware to call back <br><br>Phone Zone Dispatch at 613-745-1576.",
-  
-    DB15: "We will dispatch a technician TODAY to investigate.",
-  
-    DB16: "We will IMMEDIATELY dispatch a technician to investigate. Here are some safety precautions until we arrive. <br><br>Open ALL Doors and Windows.<br>-Leave the building<br>- If there are other people living at the premise, knock on their doors to advise them to also evacuate but DO NOT use a phone to call them<br>-Seek Medical Attention if experiencing headache, nausea, dizziness, tiredness or flu-like symptoms<br>-Call emergency if necessary<br><br>*Ensure that someone will be on site to meet the technician<br>*When technician arrives gas will be shut off if they cannot gain access.<br><br>PLEASE NOTE:<br>If the technician is expecting someone to answer the door and nobody does, the technician may ask the Fire Department to gain access to the premise.<br><br>Phone Zone Dispatch at 613-745-1576.",
-  
-    DB17: "If you are a Fortis CUSTOMER, then please check your telephone listings for a licensed gas fitter, and arrange to have your gas appliances inspected and serviced if required.",
-  
-    DB18: "We will provide you with literature on CO and appliance maintenance.",
-  
-    DB19: "We will IMMEDIATELY dispatch a technician to investigate. <br><br>Keep people and sources of ignition clear of the excavation and escaping gas until our Technician arrives. <br><br>Phone Zone Dispatch at 613-745-1576.",
-  
-    DB20: "We will IMMEDIATELY dispatch a technician to investigate. <br><br>Keep people and sources of ignition clear of the excavation and any escaping gas until our Technician arrives. <br><br>Leave everything as is; if it is on, leave it on, if it is off, leave it off. <br><br>DO NOT:<br>-Light matches or cigarettes<br>-Turn your lights ON or OFF<br>-Use any appliances<br>-Use the telephone after this call at the location of the emergency <br><br>DO:<br>-Open doors and windows to allow ventilation<br>-Leave building, ensuring someone will be on-site to meet the technician and provide access <br><br>When tech arrives gas will be shut off if they cannot gain access. <br><br>Phone Zone Dispatch at 613-745-1576.",
-  
-    DB22: "Please contact BC ONE Call at 1 800 474 6886.",
-  
-    DB23: "We will dispatch a technician TODAY to investigate. <br> Here are some safety precautions to follow until we arrive. <br><br>DO NOT: <br>-Light matches or cigarettes <br>-Use anything that could create a spark or flame, including vehicles, telephones, and machinery <br>-Undertake any corrective action <br><br>DO: <br>-Stay clear of the affected area, but remain nearby in a safe location to meet the technician <br>-Call back with a contact telephone number if evacuating the area <br>-Keep doors and windows closed to prevent gas from entering the building <br>-If you begin to smell gas inside the building you should ventilate on the opposite side from where the gas leak is located. <br><br>When tech arrives gas will be shut off at the meter if they cannot gain access.",
-  
-    DB25: "We will IMMEDIATELY dispatch a technician to investigate. <br>If you are STILL at the location AND YOU ARE NOT A meter reader, FortisBC contractor or dispatcher, then here are some safety precautions to follow until we arrive. <br><br>DO NOT: <br>-Light matches or cigarettes <br>-Use anything that could create a spark or flame, including vehicles, telephones, and machinery <br>-Undertake any corrective action <br><br>DO: <br>-Stay clear of the affected area, but remain nearby in a safe location to meet the technician <br>-Call back with a contact telephone number if evacuating the area <br>-Keep doors and windows closed to prevent gas from entering the building <br>-If you begin to smell gas inside the building, you should ventilate on the opposite side from where the odour is coming from <br><br>When tech arrives gas will be shut off if they cannot gain access. <br><br>Phone Zone Dispatch at 613-745-1576.",
-  
-    DB26: "We will dispatch a technician TODAY to investigate.",
-  }
+  DB8: "CSR:  Confirm with caller if multiple appliances are not working or if a single appliance is not working\n- If multiple appliances are not working, cancel form and create meter investigate “urgent” service order.  (If after hours (4:00 pm – 7:30 am), phone Dispatch at 613-745-1576 option 7)\n- If single appliance only, advise customer:   Check your telephone listings for a licensed gas fitter, and arrange to have your gas appliances inspected and serviced if required.",
 
-var emailSubject = "Code: ";
-function appendToSubjectLine(text) {
-    // emailSubject = $('#email_subject').val();
-    emailSubject += text;
-    $('#email_subject').val(emailSubject);
-  }
+  DB9: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate.  The technician will make the situation safe; however, they will not make repairs to appliances.  Here are some safety precautions to follow until we arrive.\n\nDO NOT:\n-Light matches or cigarettes\n-Turn your electric lights ON or OFF\n-Use your electric appliances\n-Use the telephone after this call at the location of the emergency\n\nDO:\n-Open doors and windows to allow ventilation\n-Leave building, ensuring someone will be on-site to meet the technician and provide access\n\n*When technician arrives gas will be shut off if they cannot gain access:\n\nIf the appliance is: a gas dryer, range, stove or fireplace - SWITCH OFF IMMEDIATELY\n\nIf a furnace:  TURN THERMOSTAT DOWN UNTIL  FURNACE SWITCHES OFF\n\nPhone Zone Dispatch at 613-745-1576",
 
-  
+  DB11: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate. The technician will make the situation safe; however, they will not make repairs to appliances.  Here are some safety precautions to follow until we arrive.\n\nCALL emergency line.\n\nDO NOT:\n-Light matches or cigarettes\n-Turn your electric lights ON or OFF\n-Use any appliances\n-Use the telephone after this call at the location of the emergency\n\nDO:\n-Open doors and windows to allow ventilation\n-Leave building, ensuring someone will be on-site to meet the technician and provide access\n\n*When technician arrives gas will be shut off if they cannot gain access.\n\nIf the appliance is: a gas dryer, range, stove or fireplace - SWITCH OFF IMMEDIATELY\n\nIf a furnace:  TURN THERMOSTAT DOWN UNTIL FURNACE SWITCHES OFF\n\nPhone Zone Dispatch at 613-745-1576",
 
-function checkAndAppend() {
-    var A1 = $('input[name=appliance_desc]:checked').val();
-    var A2 = $('input[name=gas_check]:checked').val();
-    var A3 = $('input[name=glass_hit]:checked').val();
-    var A4 = $('input[name=smoke_check]:checked').val();
-    var QF1 = $('input[name=response_type]:checked').val();
-    var Q2 = $('input[name=emergency_nature]:checked').val();
-    var M1 = $('input[name=meter_desc]:checked').val();
-    var M2 = $('input[name=reg_check]:checked').val();
-    var M3 = $('input[name=gas_meter_check]:checked').val();
+  DB13: "Advise Caller - We will dispatch a technician TODAY to investigate.  The technican will make the situation safe; however, they will not make repairs to appliances.  Here are some safety precautions to follow until we arrive.\n\nTurn the thermostat to the lowest setting.\n\n*Ensure that someone will be on site to meet the technician\n*When technician arrives gas will be shut off if they cannot gain access.",
 
-    // Logic for appending based on conditions
-    if (QF1 === 'response_type1') {
-      appendToSubjectLine('ZE07 ');
-    }
-    if (Q2 === 'emergency_nature2') {
-        console.log(A1, A4)
-        if (A1 === 'appliance_desc5' && A2 === 'gas_check2' && A4 === 'smoke_check2') {
+  DB14: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate.\n\nIf Fire Department asks for an ETA, advise them that (IRM Dispatch)(SR6707)will call them back and make sure  (IRM Dispatch)(SR6707) is aware to call back\n\nPhone Zone Dispatch at 613-745-1576",
 
-            appendToSubjectLine('ZE08 ');
-        }
-    }
+  DB15: "Advise Caller - We will dispatch a technician TODAY to investigate.\n\nCSR: CANCEL FORM AND CREATE METER INVESTIGATE “URGENT”  SERVICE ORDER ",
 
-    else if ((A1 === 'appliance_desc1' || A1 === 'appliance_desc2' || A1 === 'appliance_desc3' || A1 === 'appliance_desc4' || A1 === 'appliance_desc6') && A2 === 'gas_check1') {
-      appendToSubjectLine('ZE07 ');
-    }
-    else if (A1 === 'appliance_desc2' && A2 === 'gas_check2') {
-        appendToSubjectLine('ZE07 ');
-      }
-    else if (A1 === 'appliance_desc4' && A2 === 'gas_check2' && A4 === 'glass_hit2') {
-        appendToSubjectLine('ZE07 ');
-    }
-  
-    else if (A3 === 'glass_hit2') {
-        appendToSubjectLine('ZE07 ');
-    } 
-  
+  DB16: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate. Here are some safety precautions until we arrive.\n\nOpen ALL Doors and Windows.\n-Leave the building\n- If there are other people living at the premise, knock on their doors to advise them to also evacuate but DO NOT use a phone to call them\n-Seek Medical Attention if experiencing headache, nausea, dizziness, tiredness or flu-like symptoms\n-Call emergency if necessary\n\n*Ensure that someone will be on site to meet the technician\n*When technician arrives gas will be shut off if they cannot gain access.\n\nPLEASE NOTE:\nIf the technician is expecting someone to answer the door and nobody does, the technician may ask the Fire Department to gain access to the premise.\n\nPhone Zone Dispatch at 613-745-1576",
 
+  DB17: "Customer Information Only\n\nPlease check your telephone listings for a licensed gas fitter, and arrange to have your gas appliances inspected and serviced if required.",
 
+  DB18: "Requestion Info about appliance safety…or responding to media/advertising:\n\nSend caller literature on CO and appliance maintenance",
 
-    if (Q2 === 'emergency_nature1' && M1 === 'meter_desc3') {
-        if (M2 === 'reg_check1') {
-            appendToSubjectLine('ZE16 ');
-        } else {
-            // Manual service order
-        }
-    }
+  DB19: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate.\n\nKeep people and sources of ignition clear of the excavation and escaping gas until our Technician arrives.\n\nPhone Zone Dispatch at 613-745-1576",
 
-    else if (Q2 === 'emergency_nature1' && M1 === 'meter_desc6') {
-        if (M3 === 'gas_meter_check1') {
-            appendToSubjectLine('Odour ');
-        } else {
-            // Manual service order
-        }
-    }
-    
-    else if (Q2 === 'emergency_nature1') {
-        if (M1 === 'meter_desc1') {
-            appendToSubjectLine('ZMR1 ');
-        }
-        if (M1 === 'meter_desc2') {
-            appendToSubjectLine('ZE16 ');
-        }
-        if (M1 === 'meter_desc4') {
-            appendToSubjectLine('ZMR2 ');
-        }
-        else if (M1 === 'meter_desc5') {
-            appendToSubjectLine('ZE16 ');
-        }
-    }
-    //   $.getscript("nonuser-pdf.js",function(){
-    //     sendEmail();
-    //     });
-    sendEmail(emailSubject);
-    }
+  DB20: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate.\n\nKeep people and sources of ignition clear of the excavation and any escaping gas until our Technician arrives.\n\nLeave everything as is; if it is on, leave it on, if it is off, leave it off.\n\nDO NOT:\n-Light matches or cigarettes\n-Turn your lights ON or OFF\n-Use any appliances\n-Use the telephone after this call at the location of the emergency\n\nDO:\n-Open doors and windows to allow ventilation\n-Leave building, ensuring someone will be on-site to meet the technician and provide access\n\nWhen tech arrives gas will be shut off if they cannot gain access.\n\nPhone Zone Dispatch at 613-745-1576",
+
+  DB22: "Please advise caller to contact BC ONE Call at 1 800 474 6886",
+
+  DB23: "Advise Caller - We will dispatch a technician TODAY to investigate.\nHere are some safety precautions to follow until we arrive.\n\nDO NOT:\n-Light matches or cigarettes\n-Use anything that could create a spark or flame, including vehicles, telephones, and machinery\n-Undertake any corrective action\n\nDO:\n-Stay clear of the affected area, but remain nearby in a safe location to meet the technician\n-Call back with a contact telephone number if evacuating the area\n-Keep doors and windows closed to prevent gas from entering the building\n-If you begin to smell gas inside the building you should ventilate on the opposite side from where the gas leak is located.\n\nWhen tech arrives gas will be shut off at the meter if they cannot gain access",
+
+  DB25: "Advise Caller - We will IMMEDIATELY dispatch a technician to investigate. (If caller is no longer at the location or is a meter reader, FortisBC contractor or dispatcher DO NOT READ OUT THE SAFETY PRECAUTIONS)  Otherwise advise the caller:  Here are some safety precautions to follow until we arrive.\n\nDO NOT:\n-Light matches or cigarettes\n-Use anything that could create a spark or flame, including vehicles, telephones, and machinery\n-Undertake any corrective action\n\nDO:\n-Stay clear of the affected area, but remain nearby in a safe location to meet the technician\n-Call back with a contact telephone number if evacuating the area\n-Keep doors and windows closed to prevent gas from entering the building\n-If you begin to smell gas inside the building, you should ventilate on the opposite side from where the odour is coming from\n\nWhen tech arrives gas will be shut off if they cannot gain access.\n\nPhone Zone Dispatch at 613-745-1576",
+
+  DB26: "Advise Caller - We will dispatch a technician TODAY to investigate",
+}
 
 $(document).ready(function() {
 
-        
   $('input[type=radio][name=response_type]').on('change', function() {
       $('input[type=radio][name!=response_type]').prop('checked', false);
       $('#building_collapse').collapse('show');
@@ -202,7 +118,6 @@ $(document).ready(function() {
 document.addEventListener('DOMContentLoaded', function() {
   // Get the modal
   var modal = document.getElementById("exampleModal");
-
 
 
   var dbKeyMapping = {
