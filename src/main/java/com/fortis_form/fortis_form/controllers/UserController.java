8ca2ccba6761daa715c18476b5dbd604a9719ddc
Isa <isa.nafieiev@gmail.com>
2023-07-18 13:35:32 -0700

    google login works (buggy)

--- src/main/java/com/fortis_form/fortis_form/controllers/UserController.java
+++ src/main/java/com/fortis_form/fortis_form/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.fortis_form.fortis_form.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
